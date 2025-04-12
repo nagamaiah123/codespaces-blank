37c5c9c6e711d4bc94c56978449faf9bd9df2467
nagamaiah123 <nagamaiah.kummari@i95dev.com>
2025-04-12 16:35:37 +0000

    added python streamlit code

---
+++ streamlit-app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Hello from python Streamlit!")
+st.write("This app is running inside GitHub Codespaces 🚀")
+st.write("This is nag's from jivainfotech. I'm using github codespaces to run basic python streamlit app on online development environment 🚀")
